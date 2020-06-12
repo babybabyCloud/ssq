351745ec74934fcba3273461cac625f2b07f96cf
babybabycloud <364847242@qq.com>
2020-06-12 18:09:22 +0800

    Optimaize dboperator. Using decorator to wrap new_session for avoiding call init_database

--- Py/Spider/dboperator/__init__.py
+++ Py/Spider/dboperator/__init__.py
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+from functools import wraps
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -7,14 +8,24 @@ from threading import Lock
 
 Base = declarative_base()
 __Session = None
+__engine = None
 __lock = Lock()
 
 
+def init_database(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        result = f(*args, **kwargs)
+        Base.metadata.create_all(__engine)
+        return result
+    return wrapper
+
+@init_database
 def new_session(url):
     global __Session
+    global __engine
     with __lock:
         if __Session is None:
-            engine = create_engine(url, echo=False)
-            __Session = sessionmaker(engine)
-            Base.metadata.create_all(engine)
+            __engine = create_engine(url, echo=False)
+            __Session = sessionmaker(__engine)
         return __Session()
