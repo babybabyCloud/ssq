1bef19e06050e3dab0002f443c85700bd0bec324
babybabyCloud <364847242@qq.com>
2020-07-16 12:29:38 +0800

    Fix unittest

--- Py/Spider/dboperator/test_DBHandler.py
+++ Py/Spider/dboperator/test_DBHandler.py
@@ -9,12 +9,12 @@ from ..dbmodels import *
 class DBHandlerTest(unittest.TestCase):
 
     @classmethod
-    def setUp(cls):
+    def setUpClass(cls):
         cls.session = dbop.new_session('sqlite:///:memory:')
         cls.dbh = DbHandler()
 
     def test_insert_base(self):
-        self.dbh.insert_base(1, [1,2,3,4,5,6], 10, datetime.datetime.now(), self.session)
+        self.dbh.insert_base(2, [1,2,3,4,5,6], 10, datetime.datetime.now(), self.session)
         self.session.commit()
         record = self.session.query(RecordBase).first()
         self.assertEqual(1, record.id)
